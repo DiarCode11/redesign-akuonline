9fb58c79b3057b71bf58b963d39b68caa16e3a53
Diar Sudiar <163659290+DiarCode11@users.noreply.github.com>
2025-07-26 19:21:17 +0800

    fix: bug dropdown lagi

--- src/components/kk-component/edit-data-section/data-perkawinan.tsx
+++ src/components/kk-component/edit-data-section/data-perkawinan.tsx
@@ -30,6 +30,7 @@ export default function DataPerkawinanSection({ title, addMode = false, onChange
             <h1 className="text-md font-semibold">{title}</h1>
             <div className="grid sm:grid-cols-2 grid-cols-1 gap-y-6 gap-x-8 mt-5">
                 <DropdownComponent
+                    getDropdownStatus={() => {}}
                     data={['Belum/Tidak kawin', 'Kawin', 'Cerai Hidup', 'Cerai Mati']}
                     label="Status Perkawinan"
                     defaultData={addMode ? "" : "Kawin"}
